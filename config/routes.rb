a94bd55d5cdd66678fc5d83d93654e997e8ab099
Adi Berkowitz <berkowitzadi@gmail.com>
2017-03-09 15:39:25 -0500

    add singup integration test, and changed form_for -> singup_path

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :enrollments
   resources :users
   root 'home#index'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
