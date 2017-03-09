6c6f332bd6b5153c139b54bb9640cd3e18667f04
Adi Berkowitz <berkowitzadi@gmail.com>
2017-03-08 19:48:05 -0500

    scaffolds for User, Course, and Enrollment

---
+++ db/migrate/20170309003744_create_enrollments.rb
@@ -0,0 +1,11 @@
+class CreateEnrollments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :enrollments do |t|
+      t.integer :user_id
+      t.integer :course_id
+      t.boolean :ta
+
+      t.timestamps
+    end
+  end
+end
