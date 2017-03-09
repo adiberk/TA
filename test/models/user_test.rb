cffc95c11cc98d209f2c0bdc5ff2f3ac630f8618
Adi Berkowitz <berkowitzadi@gmail.com>
2017-03-08 20:04:58 -0500

    add tests and validity to model and test model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,18 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(first_name: "Example", last_name: "User", username: "euser", email: "user@example.com", university_id:"1")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.first_name = "     "
+    @user.last_name = "     "
+    assert_not @user.valid?
+  end
+
 end
