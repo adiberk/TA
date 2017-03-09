534f335b856e217877cdbe4aca9c3a6d041ed838
Adi Berkowitz <berkowitzadi@gmail.com>
2017-03-09 13:05:25 -0500

    add homepage, using application.html.erb as well as _header and _footer.  All tests passing still

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :courses
   resources :enrollments
   resources :users
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
