6b3d3ea152499ae153ea59a0e3b84016f38e6e2d
Nick April <napril@brandeis.edu>
2017-03-11 19:45:31 -0500

    sidebar fixes and course page

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170310061851) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
+    #t.string   "teacher"
     t.integer  "major_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
