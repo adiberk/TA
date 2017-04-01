903ab0392aa5c19cea466fa68fa05095b0846b82
Adi Berkowitz <berkowitzadi@gmail.com>
2017-04-01 17:41:06 -0400

    set root path and render id"

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  put '/confirm_appointment', to: 'appointments#confirm_app'
+  post '/confirm_appointment', to: 'appointments#confirm_app'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
