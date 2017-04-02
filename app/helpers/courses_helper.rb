e050fb363aac052aa4ae3dc1515f98018eb6f21c
Nick April <napril@brandeis.edu>
2017-04-02 12:38:03 -0400

    add course links to profile page

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -4,14 +4,29 @@ module CoursesHelper
       if my_courses.length == 0
         return "Not registered for courses"
       end
-	  str = "<li>#{my_courses[0].name}</li> "
+    name = my_courses[0].name
+    id = my_courses[0].id
+
+    #str = "<li> <%= link_to 'name', :controller => 'courses', :action =>'show', :course_id=>'id', :course_name=>'name' %> </li>"
+    str = "<li> test </li>"
+
 	  count = 1
       while count < my_courses.length do
-      	str+="<li>#{my_courses[count].name}</li>"
+
+        name = my_courses[count].name
+        id = my_courses[count].id
+
+        #str += "<li> <%= link_to 'name', :controller => 'courses', :action =>'show', :course_id=>'id', :course_name=>'name' %> </li>"
+        str += "<li> poop </li>"
+
       	count+=1;
       end
       str.html_safe
   end
+
+
+
+
 
   def get_student_list(course)
     enrolled = course.enrollments
