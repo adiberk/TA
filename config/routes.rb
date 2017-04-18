f3eefe6b82f046e670e1bf367e642026293ad5ee
Nick April <napril@brandeis.edu>
2017-04-17 20:54:17 -0400

    signin, course, and sidebar style edits

--- config/routes.rb
+++ config/routes.rb
@@ -4,25 +4,18 @@ Rails.application.routes.draw do
   resources :enrollment_tas
   resources :appointments
   resources :officehours
-  # get 'sessions/new'
-
   resources :chat_rooms
+  resources :courses
+  resources :enrollments
+  resources :users
+
+  root 'users#show'
 
   mount ActionCable.server => '/cable'  
 
   get 'home/index'
-
-  resources :courses
-  resources :enrollments
-  resources :users
-  root 'home#index'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
   get '/talist',   to: 'enrollment_tas#talist'
   get '/taprofile',   to: 'users#taprofile'  
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
   post '/confirm_appointment', to: 'appointments#confirm_app'
   post '/unconfirm_appointment', to: 'appointments#unconfirm_app'
   post '/request_appointment', to: 'appointments#request_app'
@@ -31,5 +24,4 @@ Rails.application.routes.draw do
 
   delete '/chat_rooms',   to: 'chat_rooms#index'
   # get '/confirm_appointment', to: 'appointments#confirm_app'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
