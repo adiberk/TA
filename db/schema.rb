8b4d924da97f980afc9452c69e9dce12733b2b69
YOUR NAME <berkowitzadi@gmail.com>
2017-04-20 20:50:14 +0000

    fix messages error on db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420183439) do
+ActiveRecord::Schema.define(version: 20170420204826) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer  "course_id"
