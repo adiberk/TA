9fdd93480f92bc07ab9797d64e4ec1e3d85a589b
Hao Wang <fjqz50@gmail.com>
2017-04-23 15:18:52 -0400

    create a animated side nav for chat

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -142,4 +142,33 @@ $( document ).ready(function() {
 });
 
 
-// Ajax for taprofile: submit the review
+// tab function
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+//side bar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
