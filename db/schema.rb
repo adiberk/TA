53cf043e916355dfae5c756c8d9ca7868ae0f00c
Nick April <napril@brandeis.edu>
2017-04-25 10:27:52 -0400

    merge with d-master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170423164441) do
 
   create_table "appointments", force: :cascade do |t|
