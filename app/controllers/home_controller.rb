4bae2925ffc885e27c94397590d793f3a1d326d5
Nick April <napril@brandeis.edu>
2017-04-25 17:01:41 -0400

    not working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,3 @@
 class HomeController < ApplicationController
-  def index
-    session[:conversations] ||= []
 
-    @users = User.all.where.not(id: current_user)
-    @conversations = Conversation.includes(:recipient, :messages)
-                                 .find(session[:conversations])
-  end
-
-  def close
-    @conversation = Conversation.find(params[:id])
-
-    session[:conversations].delete(@conversation.id)
-
-    respond_to do |format|
-      format.js
-    end
-  end
 end
