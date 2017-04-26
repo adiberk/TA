3fe60a490bb5b7a58e7217a78e2b8d796bbefc11
YOUR NAME <berkowitzadi@gmail.com>
2017-04-26 21:20:49 +0000

    added smtp settngs etc. to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,22 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { :host => 'taondemand.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'yoursite.herokuapp.com'
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "brandeis.taondemand",
+  password: "foobar96"
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
