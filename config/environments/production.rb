a312390f0c6704f034335aa23ee620f7f54f2b9e
YOUR NAME <berkowitzadi@gmail.com>
2017-04-26 22:49:15 +0000

    comment out something in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,8 +17,8 @@ Rails.application.configure do
     domain: "taondemand.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "app64858160@heroku.com",
-    password: "jwfqgv1b0232"
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
   }
   # Code is not reloaded between requests.
   config.cache_classes = true
