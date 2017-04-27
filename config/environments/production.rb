29fc23b10ea4c3989f1f261f02edeb335887bfc5
YOUR NAME <berkowitzadi@gmail.com>
2017-04-27 16:06:22 +0000

    add debug for heroku in production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.log_level = :debug
   config.action_mailer.default_url_options = { :host => 'taondemand.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
