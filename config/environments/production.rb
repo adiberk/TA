e11641831a3ccce34affdd5af2e31fa7aca67128
YOUR NAME <berkowitzadi@gmail.com>
2017-04-27 15:27:53 +0000

    prepare production file for heroku upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,20 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { :host => 'taondemand.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
+  config.action_mailer.smtp_settings = {
+  address: "smtp.sendgrid.com",
+  port: 587,
+  domain: "taondemand.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "brandeis.taondemand@gmail.com",
+  password: "gmail.com"
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
