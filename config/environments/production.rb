e84b58ecbd07da5a8d84e8c582561f55022e0a82
YOUR NAME <berkowitzadi@gmail.com>
2017-04-27 16:39:19 +0000

    change domain to heroku.com on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.sendgrid.com",
   port: 587,
-  domain: "taondemand.herokuapp.com",
+  domain: "heroku.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: "app64858160@heroku.com",
