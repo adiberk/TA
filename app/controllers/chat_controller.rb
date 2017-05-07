c995103cb18da5a835ff0d86045ab761698a308e
Nick April <napril@brandeis.edu>
2017-05-07 14:38:28 -0400

    final chat

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -8,32 +8,32 @@ class ChatController < ApplicationController
 
   	@user = current_user
   	@course_list = @user.courses
-  	@ta_list = Array.new
+  	@ta_list = Hash.new
 
   	@course_list.each do |course|
   		course.tas.each do |ta|
-  			@ta_list.push(ta)
+  			if !@ta_list.has_key?(ta)
+          @ta_list[ta] = course.abbrev
+        end
   		end
   	end
 
-	@ta_list = @ta_list.uniq
-
-
 	@student_list = Array.new
-	@course_list.each do |course|
-		course.enrollments.each do |e|
-			if e.user_id == current_user.id
-				#skip
-			else
-				@student_list.push(@users.find(e.user_id))
-			end
-		end
-	end
+  if @user.tacourses != nil
+    @course_list = @user.tacourses
+  	@course_list.each do |course|
+  		course.enrollments.each do |e|
+        
+  			if e.user_id == current_user.id
+  				#skip
+  			else
+  				@student_list.push(@users.find(e.user_id))
+  			end
+  		end
+  	end
+  end
 
 	@student_list = @student_list.uniq
 
   end
-
-
-
 end
